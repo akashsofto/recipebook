d06faed3bb58f047aecc1fb71cfd28ea91bb2243
Akash Kumar <akash.kumar@softobiz.com>
2022-08-19 15:04:58 +0530

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,24 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.component';
+import { RecipeEditComponent } from './recipes/recipe-edit/recipe-edit.component';
+import { RecipeStartComponent } from './recipes/recipe-start/recipe-start.component';
+import { RecipesComponent } from './recipes/recipes.component';
+import { ShoppingListComponent } from './shopping-list/shopping-list.component';
 
-const routes: Routes = [];
+const appRoutes: Routes = [
+  {path:'',redirectTo:'/recipes',pathMatch:'full'},
+  {path:'recipes',component:RecipesComponent,children:[
+    {path:'',component:RecipeStartComponent},
+    {path:':id/edit',component:RecipeEditComponent},
+    {path:':id',component:RecipeDetailComponent},
+    {path:'new',component:ShoppingListComponent}
+  ]},
+  {path:'shopping-list',component:ShoppingListComponent}
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(appRoutes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
